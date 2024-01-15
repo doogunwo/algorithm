8f34eff6b1875b05edfeb98309a67c4dede84724
도건우.io <87505243+doogunwo@users.noreply.github.com>
2024-01-16 03:04:02 +0900

    [Silver V] Title: 숫자 카드, Time: 552 ms, Memory: 133108 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,16 +1,23 @@
 import sys
-from collections import Counter
 
 input = sys.stdin.readline
 
+def count_elements(iterable):
+    element_count = {}
+    for element in iterable:
+        if element in element_count:
+            element_count[element] += 1
+        else:
+            element_count[element] = 1
+    return element_count
+
 n = int(input())
-
-n_list = list(map(int,input().split()))
-
+n_list = list(map(int, input().split()))
 m = int(input())
+m_list = list(map(int, input().split()))
 
-m_list = list(map(int,input().split()))
+# 직접 만든 count_elements 함수 사용
+card_count = count_elements(n_list)
 
-card_count = Counter(n_list)
-result = [card_count[target] for target in m_list]
+result = [card_count.get(target, 0) for target in m_list]
 print(" ".join(map(str, result)))
