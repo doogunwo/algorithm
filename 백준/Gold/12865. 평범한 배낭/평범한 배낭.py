c52d66b8576c1c7e3cb7a523fc9e2e3a414a3a68
도건우.io <87505243+doogunwo@users.noreply.github.com>
2024-02-05 23:15:21 +0900

    [Gold V] Title: 평범한 배낭, Time: 5044 ms, Memory: 227172 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,18 @@
+import sys
+(N, K) = map(int, sys.stdin.readline().split())
+item = [[0, 0]]
+for i in range(1, N + 1):
+    item.append(list(map(int, sys.stdin.readline().split())))
+
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        if j >= item[i][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-item[i][0]] + item[i][1])
+        else:
+            dp[i][j] = dp[i-1][j] 
+            
+            
+print(dp[N][K])
+
