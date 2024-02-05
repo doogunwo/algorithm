404e05cfd5c1c0b8f72c388df157a635dcf17c44
도건우.io <87505243+doogunwo@users.noreply.github.com>
2024-02-05 23:14:39 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		int n;
+		cin >> n;
+		sum = sum + (n * n);
+	}
+	cout << sum % 10;
+}
