fa97d62671c3fb905638ec4214b8739b68ad0e60
도건우.io <87505243+doogunwo@users.noreply.github.com>
2024-02-05 23:13:34 +0900

    [Silver III] Title: 1로 만들기, Time: 640 ms, Memory: 38676 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,19 @@
+x = input()
+x = int(x)
+
+# x를1로만들자
+d = [0]*(x+1)
+
+
+for i in range(2,x+1):
+
+
+    d[i] = d[i-1] +1
+
+    if i%3==0:
+        d[i] = min(d[i],d[i//3]+1)
+    if i%2==0:
+        d[i] = min(d[i],d[i//2]+1)
+        
+
+print(d[x])
