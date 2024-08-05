d89266b8d12a88060093d52bb032ef3892f39c86
도건우.io <87505243+doogunwo@users.noreply.github.com>
2024-08-05 21:36:26 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.cc
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -1,21 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> // std::max_element
-using namespace std;
+#include <stdio.h>
 
-int main() {
-    vector<int> numbers(9);
-
-    // 9개의 숫자를 입력받음
-    for (int i = 0; i < 9; i++) {
-        cin >> numbers[i];
+int main(){
+  int max = 1, a, i;
+  int A[10];
+  
+  for(i=0; i<9; i++){
+    scanf("%d", &A[i]);
+    if(max <= A[i]){
+      max = A[i];
+      a=i;
     }
-
-    // 최대값의 반복자를 찾음
-    auto maxIt = max_element(numbers.begin(), numbers.end());
-    int maxIndex = distance(numbers.begin(), maxIt);
-    // 최대값과 그 인덱스 출력
-    cout << *maxIt << endl << maxIndex + 1 << endl;
-
-    return 0;
+  }
+  printf("%d\n",max);
+  printf("%d\n",a+1);
 }
